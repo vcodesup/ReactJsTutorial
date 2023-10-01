1572d471dc06aa499c716ed2952f827585793074
vishal20156 <vishal.ramnani171@gmail.com>
2023-10-01 17:41:22 +0530

    Upto Episode 3

---
+++ README.md
@@ -0,0 +1,28 @@
+#Namaste React!
+
+
+
+
+#Parcel - Heavy Work Bundler!
+- Dev Build
+- Local Server
+- HMR ( Hot Module Replacement ) 
+- File Watching Algorithm - Written in CPP - Watches on the changes in the file
+- Caching - Faster Builds! Stores Cache in Project Folder
+- Image Optimization
+- Minification of files - Production Build
+- Compress the files.
+- Consistent Hashing
+- Differential Bundling - To Support Older Browsers [ Different Bundles for Different browsers ]
+- Code Splitting
+- Better Error Interface
+- Https Support for Test
+- Tree Shaking Algorithm - Removes unused code
+- Different builds - dev and prod
+
+
+When parcel generates a build it stores the code in dist
+
+
+
+browserList ==> Configuration to make our apps work for specific versions, at any level(country too)
--- app.js
+++ app.js
@@ -1,20 +1,96 @@
-    // Create a element
-    const headingElement = React.createElement("h1","","Hi");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+/* 
+Episode 1
+ // Create a element
+const headingElement = React.createElement("h1","","Hi");
+ 
+//Create a root for dom manipulations
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//Render the element on the root
+root.render(headingElement);
+
+
+
+
+//Nested Elements Creation
+const divParent = React.createElement("div",{id:"Hi"},[
+    React.createElement("h1",{id:"heading"},"This is a Namaste React"),
+    React.createElement("h1","","This is a child 1") 
+]);
+
+//Render Nested Elements on root
+root.render(divParent);
+
+
+*/
+//Episode 3
+
+var ele = 9;
+
+const Title = () => (
+    <h1>Namaste JS Tutorial</h1>
+);
+
+
+const jsxHeading = (
+<div>
+<h1 id='heading'>This is heading using JSX {ele}</h1>
+<Title/>
+</div>
+);
+
+console.log(jsxHeading)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//if we write in one line we dont need to write return
+const fn = () => true;
+
+
+
+const Content = () => {
+    return <h1>This will be a amazing tutorial to learn and explore react js and be a master in it </h1>
+};
+
+
+
+const Heading = () => (
+    <div>
+        {Title()}
+        <Title/>
+        <Title></Title>
+        <h2 className="red-class">I am learning React JS From Scratch  </h2>
+        <Content/>
+    </div>
+);
+
+
+
+const CreateReactToJsx = () => {
     
-    //Create a root for dom manipulations
-    const root = ReactDOM.createRoot(document.getElementById("root"));
+    // React.createElement("h1",{},"This is a create React Weird Method to create elements")
+    return <h1 className = "red-class">This is a create React Weird Method to create elements</h1>
+    
 
-    //Render the element on the root
-    root.render(headingElement);
+}
+//We can also write above as
+
+const HeadingOneLinerComponent = () => <h1>Heading in one lines</h1>
+
+
+root.render(<Heading/>);
 
 
 
 
-    //Nested Elements Creation
-    const divParent = React.createElement("div",{id:"Hi"},[
-        React.createElement("h1",{id:"heading"},"This is a child 1"),
-        React.createElement("h1","","This is a child 1") 
-    ]);
 
-    //Render Nested Elements on root
-    root.render(divParent);
+
+
+
+
+
+
+
+
+
